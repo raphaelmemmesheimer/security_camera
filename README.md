8b49a14773a7f174cd97497d1c421d271ed84330
Raphael M <raphael@airglow.org>
2013-06-05 23:40:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Security Camera
+
+This simple script can be used with all camera-servers which serve images via 
+an url. For instance IP Webcam for Android.
+
+* Place an alarm sound named alarm.wav in the same directory
+* Start script like `python main.py <camera_stream_url>`
+
+Images are stored at the script location
---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2, urllib, Image, time, pyglet, sys
+
+backsub = cv2.BackgroundSubtractorMOG()
+alarmSound = pyglet.resource.media("alarm.wav", streaming = False)
+imgURL = sys.argv[1] # for instance via ip cam app
+
+while True:
+	filename = time.strftime("%Y%m%d%H%M%S.jpg", time.localtime()) # save image every 1 second
+	urllib.urlretrieve(imgURL,filename)
+	img = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE);
+	fgmask = backsub.apply(img,None, 0.01)
+	contours = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+	for contour in contours[0]:
+		(x,y,w,h) = cv2.boundingRect(contour)
+		if w > 10 and h > 10:
+			cv2.rectangle(img, (x,y), (x+w,y+h), (255, 0, 0), 2)
+			alarmSound.play()
+	
+	cv2.imshow("image", img)
+	cv2.waitKey(1)
